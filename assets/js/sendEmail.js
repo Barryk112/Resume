72daa8c73767035cdc34acc9ba11619e7810f82c
Baz <111312411+Barryk112@users.noreply.github.com>
2023-03-29 18:21:08 +0000

    Fixed bug where email was not being sent, found that the form in contact.html was not layed out correctly, all works now

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,20 +1,19 @@
 function sendMail(contactForm) {
-    emailjs.sendForm("service_zt9gr84", "resume", {
+
+    var templateParams = {
         from_name: contactForm.name.value,
-        project_request: contactForm.projectsummary.value,
-        from_email: contactForm.emailaddress.value
-        
-    })
+        from_email: contactForm.emailaddress.value,
+        project_request: contactForm.projectsummary.value
+    }
+
+    emailjs.send("service_zt9gr84", "resume", templateParams)
     .then(
         function(response) {
             console.log("SUCCESS", response);
         },
         function(error) {
-            console.log("FAILED",error);
+            console.log("FAILED", error);
         }
     );
-
-    return false;
+    return false;  // To block from loading a new page
 }
-
-console.log("shit");
